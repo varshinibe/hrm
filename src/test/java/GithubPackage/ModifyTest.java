8ad165f12e27d82701bd720beaac59904e38ad33
vinod kumar <vinod kumar@DESKTOP-SP7C17C>
2022-09-27 15:27:31 +0530

    commit2

---
+++ src/test/java/GithubPackage/ModifyTest.java
@@ -0,0 +1,17 @@
+package GithubPackage;
+
+import org.testng.annotations.Test;
+
+public class ModifyTest {
+	@Test
+	public void create()
+	{
+		System.out.println("-created-");
+	}
+	@Test
+	public void modify() {
+		System.out.println("---modifies---");
+	}
+	
+
+}
