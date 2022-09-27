93f654017c0dd389bc927a7726cb285b44186df4
Dell <Dell@DESKTOP-BE5FK59>
2022-09-27 12:47:44 +0530

    First commit

---
+++ src/test/java/GithubPackage/CreateTest.java
@@ -0,0 +1,19 @@
+package GithubPackage;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+
+	@Test
+	public void create()
+	{
+		System.out.println("---created---");
+	}
+	
+	
+	
+	
+	
+	
+	
+}
